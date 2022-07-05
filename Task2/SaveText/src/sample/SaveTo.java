2d5738ac2b1dc3e07cccc658f0ce5158dea7ca09
Vladimir <vgorodnyuk@gmail.com>
2022-07-05 13:18:12 +0300

    Delete Task2/SaveText directory

--- Task2/SaveText/src/sample/SaveTo.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-import java.lang.annotation.*;
-import java.lang.annotation.RetentionPolicy;
-@Retention(RetentionPolicy.RUNTIME)
-public @interface SaveTo {
-   String path();
-}
